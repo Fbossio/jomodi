961a21b58c659b709094b026b55ab70f6a479b5c
Fbossio <felixbossio@gmail.com>
2023-11-18 04:59:00 -0500

    chore: create connection with postgres database

--- jomodi-api/src/app.module.ts
+++ jomodi-api/src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BannerGalleryModule } from './banner-gallery/banner-gallery.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BannerGalleryModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BannerGalleryModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
