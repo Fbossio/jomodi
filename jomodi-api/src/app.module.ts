4027ef5c513b8e2fd48137d5552d4400417c2157
Fbossio <felixbossio@gmail.com>
2023-11-24 04:21:53 -0500

    feat: add users table

--- jomodi-api/src/app.module.ts
+++ jomodi-api/src/app.module.ts
@@ -6,6 +6,7 @@ import { BannerGalleryModule } from './banner-gallery/banner-gallery.module';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { CategoryModule } from './category/category.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CategoryModule } from './category/category.module';
     DatabaseModule,
     ProductsModule,
     CategoryModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ jomodi-api/src/users/schemas/users.schema.ts
@@ -0,0 +1,36 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP()',
+  })
+  updatedAt: Date;
+}
