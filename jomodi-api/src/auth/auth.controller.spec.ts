b283abe48df1b5d1755369584f2a77d752734eee
Fbossio <felixbossio@gmail.com>
2023-11-30 01:14:22 -0500

    test: create unit tests for auth service and auth controller

--- jomodi-api/src/auth/auth.controller.spec.ts
+++ jomodi-api/src/auth/auth.controller.spec.ts
@@ -1,20 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            async signin(ssigninDto: SigninDto) {
+              return {};
+            },
+            async signUp(user: CreateUserDto) {
+              return {};
+            },
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('Sign Up', () => {
+    it('should return a user', async () => {
+      const user = {
+        firstName: 'Test',
+        lastName: 'User',
+        email: 'test@user.com',
+        password: 'password',
+      };
+      jest.spyOn(authService, 'signUp').mockResolvedValue(user as any);
+      expect(await controller.signup(user)).toBe(user);
+    });
+  });
+  describe('Sign In', () => {
+    it('should return a token', async () => {
+      const signinDto = {
+        email: 'test@user.com',
+        password: 'password',
+      };
+      jest.spyOn(authService, 'signin').mockResolvedValue('token' as any);
+      expect(await controller.signin(signinDto)).toBe('token');
+    });
   });
 });
--- jomodi-api/src/auth/auth.service.spec.ts
+++ jomodi-api/src/auth/auth.service.spec.ts
@@ -1,18 +1,90 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
+// import { SigninDto } from './dto/signin.dto';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            async findOneByEmail(email: string) {
+              return {
+                id: 1,
+                email: email,
+                password: 'password',
+              };
+            },
+            async create(user: CreateUserDto) {
+              return {};
+            },
+            async checkPassword(password: string, hashedPassword: string) {
+              return true;
+            },
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            async sign(payload: any) {
+              return 'token';
+            },
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('validateUser', () => {
+    it('should validate user credentials', async () => {
+      const email = 'test@example.com';
+      const password = 'password';
+      jest
+        .spyOn(usersService, 'findOneByEmail')
+        .mockResolvedValue({ email, password } as any);
+      const result = await authService.validateUser(email, password);
+      expect(result).toEqual({ email, password });
+    });
+  });
+
+  describe('signin', () => {
+    it('should sign in a user', async () => {
+      const user: SigninDto = {
+        email: 'test@example.com',
+        password: 'password',
+      };
+      const result = await authService.signin(user);
+      expect(typeof result).toBe('object');
+      expect(result).toHaveProperty('access_token');
+    });
+  });
+
+  describe('signUp', () => {
+    it('should create a new user', async () => {
+      const user: CreateUserDto = {
+        firstName: 'Test',
+        lastName: 'User',
+        email: 'test@example.com',
+        password: 'password',
+      };
+
+      const result = await authService.signUp(user);
+      expect(typeof result).toBe('object');
+      expect(result).toHaveProperty('access_token');
+    });
   });
 });
