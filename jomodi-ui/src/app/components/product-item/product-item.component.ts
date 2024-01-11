a5685e9fd18b6c7ee7c364bc490442c970936a91
Fbossio <felixbossio@gmail.com>
2024-01-11 00:14:02 -0500

    feat: display quantity in product list view

--- jomodi-ui/src/app/components/product-item/product-item.component.ts
+++ jomodi-ui/src/app/components/product-item/product-item.component.ts
@@ -10,11 +10,19 @@ import { AppState } from '../../state/app.state';
 })
 export class ProductItemComponent {
   @Input() product:any = {};
+  maxQuantity: number = 0;
+  quantityArray: number[] = [];
+  selectedQuantity = 1;
 
   constructor(private store: Store<AppState>) { }
 
   handleClick() {
     this.store.dispatch(setCurrentItem({ item: this.product }));
+  }
+
+  ngOnInit() {
+    this.maxQuantity = this.product && this.product.stock >= 20 ? 20 : this.product?.stock || 0;
+    this.quantityArray = Array.from({ length: this.maxQuantity }, (_, i) => i + 1);
   }
 
 }
