9f47ec44052d895343fa14fba5b6ca6a2a8f556c
Fbossio <felixbossio@gmail.com>
2024-03-05 03:46:40 -0500

    feat: implement banner list admin view

--- jomodi-ui/src/app/components/manage-banner/manage-banner.component.ts
+++ jomodi-ui/src/app/components/manage-banner/manage-banner.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { loadBanners } from '../../state/actions/banner.actions';
+import { AppState } from '../../state/app.state';
 
 @Component({
   selector: 'app-manage-banner',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-banner.component.css']
 })
 export class ManageBannerComponent {
+
+  banners$: Observable<any> = new Observable<any>();
+
+  constructor(private store: Store<AppState>,) {}
+
+  ngOnInit() {
+    this.banners$ = this.store.select(state => state.banner.banners);
+    if (!this.banners$) {
+      this.store.dispatch(loadBanners())
+    }
+
+  }
+
+  edit(id: number) {
+    console.log('Edit banner with id: ', id)
+  }
 
 }
