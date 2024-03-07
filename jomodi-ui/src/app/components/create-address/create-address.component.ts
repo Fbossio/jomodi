7391952c4a5049473833f8296fe4daa66c2671f5
Fbossio <felixbossio@gmail.com>
2024-03-07 15:25:57 -0500

    feat: implement create address

--- jomodi-ui/src/app/components/create-address/create-address.component.ts
+++ jomodi-ui/src/app/components/create-address/create-address.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
+import { alert } from '../../utils/alert';
 
 @Component({
   selector: 'app-create-address',
@@ -7,4 +13,52 @@ import { Component } from '@angular/core';
 })
 export class CreateAddressComponent {
 
+  private subscription = new Subscription();
+  form!: FormGroup;
+  formSubmitted: boolean = false;
+  currentUser: any = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private usersService: UsersService,
+    private authService: AuthService,
+    private router: Router
+    ) {}
+
+    isLoggedIn() {
+      return this.authService.isLoggedIn();
+    }
+
+    getCurrentUser() {
+      if (this.isLoggedIn()) {
+        return this.authService.currentUser;
+      }
+      return null;
+    }
+
+  ngOnInit() {
+    this.currentUser = this.getCurrentUser();
+    this.form = this.fb.group({
+      'address': ['', Validators.compose([Validators.required])],
+      'city': ['', Validators.compose([Validators.required])],
+      'zip': ['', Validators.compose([Validators.required])],
+      'country': ['', Validators.compose([Validators.required])],
+    })
+  }
+
+  createAddress(address: any) {
+    this.formSubmitted = true;
+    if (this.form.invalid) return;
+    this.subscription.add(this.usersService.createAddress(address, this.authService.getHeaders(), this.currentUser?.sub as string).subscribe(
+      (res: any) => {
+        alert('Success', 'Address created successfully', 'success');
+        this.form.reset();
+        this.router.navigate(['/profile/manage-addresses']);
+      },
+      (error: any) => {
+        alert('Error', 'Address cannot be created', 'error');
+        console.error(error);
+      }
+    ));
+  }
 }
