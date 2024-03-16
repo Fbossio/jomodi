dd4238334197a292459def2302d2ad2a690e3d53
Fbossio <felixbossio@gmail.com>
2024-03-16 14:53:26 -0500

    chore: fix bundle size error

---
+++ jomodi-ui/src/environments/envoronment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_URL: process.env['API_URL'] || 'http://localhost:3000',
+  stripe: {
+    publicKey: process.env['STRIPE_PUBLIC_KEY'],
+  }
+};
