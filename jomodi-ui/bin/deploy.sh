e956690576c34f119727721c78c62b23cd077d5e
Fbossio <felixbossio@gmail.com>
2024-03-18 22:51:39 -0500

    fix: change ui entry point location

--- jomodi-ui/bin/deploy.sh
+++ jomodi-ui/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./dist s3://jomodi-website-399514960763/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://jomodi-website-399514960763/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/jomodi-ui/index.html s3://jomodi-website-399514960763/
