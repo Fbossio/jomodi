07af5de686d9fc216ca9b8842db96b16c152fcb6
Fbossio <felixbossio@gmail.com>
2024-03-19 05:49:02 -0500

    chore: change api url to production v4

--- jomodi-ui/src/environments/envoronment.production.ts
+++ jomodi-ui/src/environments/envoronment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: process.env['API_URL'] || 'http://localhost:3000',
+  API_URL: process.env['API_URL'] || 'jomodi-api-dev.us-east-1.elasticbeanstalk.com',
   stripe: {
     publicKey: process.env['STRIPE_PUBLIC_KEY'],
   }
