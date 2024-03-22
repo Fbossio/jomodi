fb5eca27b3ea9464db1786ae65f00392e536c9d5
Fbossio <felixbossio@gmail.com>
2024-03-21 22:37:49 -0500

    feat: include models for signin and create address in documentation

--- jomodi-api/src/auth/dto/signin.dto.ts
+++ jomodi-api/src/auth/dto/signin.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class SigninDto {
+  @ApiProperty({ example: 'test@user.com' })
   @IsEmail()
   email: string;
+  @ApiProperty({ example: 'password123' })
   @IsString()
   password: string;
 }
--- jomodi-api/src/users/dto/create-address.dto.ts
+++ jomodi-api/src/users/dto/create-address.dto.ts
@@ -17,7 +17,7 @@ export class CreateAddressDto {
   @ApiProperty({ example: '88005' })
   @IsString()
   zip: string;
-  @ApiProperty({ example: 'New Mexico' })
+  @ApiProperty({ example: 'United States' })
   @IsString()
   country: string;
   @IsOptional()
